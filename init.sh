d339133802a374846adc659a77433be0eca65b03
Tricia Bogen <tricia-github@cernu.us>
2020-12-06 21:37:26 -0800

    do more init

--- init.sh
+++ init.sh
@@ -17,14 +17,33 @@ cat > "$1"/part1/main.go <<EOF
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"strings"
 )
 
 func main() {
 	input, err := ioutil.ReadFile("$1.input")
-  if err != nil {
-  	panic(err)
-  }
-  lines := strings.Split(strings.TrimSpace(string(input)), "\n")
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
+	fmt.Println(lines)
 }
 EOF
+go fmt "$1/part1/main.go"
+
+for part in part1 part2; do
+  cat > .idea/runConfigurations/"$1$part".xml <<EOF
+<component name="ProjectRunConfigurationManager">
+  <configuration default="false" name="$1$part" type="GoApplicationRunConfiguration" factoryName="Go Application">
+    <module name="aoc20" />
+    <working_directory value="\$PROJECT_DIR$" />
+    <kind value="FILE" />
+    <filePath value="\$PROJECT_DIR\$/$1/$part/main.go" />
+    <directory value="\$PROJECT_DIR\$" />
+    <method v="2" />
+  </configuration>
+</component>
+EOF
+done
