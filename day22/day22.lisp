7c3dee0c8f4bf99280cbdf855b92a8acdda2525b
Tricia Bogen <tricia-github@cernu.us>
2020-12-21 22:16:52 -0800

    refactor to cond

--- day22/day22.lisp
+++ day22/day22.lisp
@@ -27,25 +27,27 @@
 (format t "Part 1: ~a~%" (score (play *p1* *p2*)))
 
 (defun recursive-play (p1 p2 &optional states)
-  (if (find (list p1 p2) states :test #'equal) (return-from recursive-play (values p1 t)))
-  (unless p1 (return-from recursive-play (values p2 'nil)))
-  (unless p2 (return-from recursive-play (values p1 t)))
-  (let* ((c1 (first p1))
-         (c2 (first p2))
-         (result
-           (if (and (< c1 (length p1))
-                    (< c2 (length p2)))
-             (multiple-value-bind (deck p1win)
-               (recursive-play (copy-seq (subseq (rest p1) 0 c1))
-                               (copy-seq (subseq (rest p2) 0 c2)))
-               p1win)
-             (> c1 c2))))
-    (if result
-      (recursive-play (append (rest p1) (list c1 c2))
-                      (rest p2)
-                      (append states (list (list p1 p2))))
-      (recursive-play (rest p1)
-                      (append (rest p2) (list c2 c1))
-                      (append states (list (list p1 p2)))))))
+  (cond
+    ((not p1) (values p2 'nil))
+    ((not p2) (values p1 t))
+    ((find (list p1 p2) states :test #'equal) (values p1 t))
+    (t
+      (let* ((c1 (first p1))
+             (c2 (first p2))
+             (result
+               (if (and (< c1 (length p1))
+                        (< c2 (length p2)))
+                 (multiple-value-bind (deck p1win)
+                   (recursive-play (copy-seq (subseq (rest p1) 0 c1))
+                                   (copy-seq (subseq (rest p2) 0 c2)))
+                   p1win)
+                 (> c1 c2))))
+        (if result
+          (recursive-play (append (rest p1) (list c1 c2))
+                          (rest p2)
+                          (append states (list (list p1 p2))))
+          (recursive-play (rest p1)
+                          (append (rest p2) (list c2 c1))
+                          (append states (list (list p1 p2)))))))))
 
 (format t "Part 2: ~a~%" (score (recursive-play *p1* *p2*)))
